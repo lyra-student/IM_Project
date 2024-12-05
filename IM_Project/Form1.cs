bc0a8e61f27106b43aa346d9a9741ab6f4704a52
Lyra Jamaica Vergara <Lyra Jamaica Vergara@LAPTOP-UQ3EN055>
2024-12-05 21:48:31 +0800

    Add project files.

---
+++ IM_Project/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IM_Project
+{
+    public partial class LOGIN : Form
+    {
+        public LOGIN()
+        {
+            InitializeComponent();
+        }
+
+        private void showPass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (showPass.Checked)
+            {
+                passwordtxt.PasswordChar = '\0'; // Show the password as plain text
+            }
+            else
+            {
+                passwordtxt.PasswordChar = '*'; // Hide the password as asterisks
+            }
+        }
+    }
+}
